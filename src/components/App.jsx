024d677f66d569d822996f759b09bbad1b80c622
Neizmailova Natalia <natata.neizmailova@gmail.com>
2023-02-20 12:51:54 +0200

    render App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+
+
+
+class App extends React.Component {
+  state = {
+    contacts: [],
+    name: ''
+  };
+
+  render () {
+    return (
+      <div
+        style = {{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        hhhhh
+      </div>
+    );
+  }
+}
+export default App;
